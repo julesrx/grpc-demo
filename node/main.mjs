464d96b0553ff9b56d522efe5f4fcd9b9de60843
julesrx <dev@julesrx.fr>
2024-03-01 11:48:17 +0100

    prettier

--- node/main.mjs
+++ node/main.mjs
@@ -1,11 +1,11 @@
 import { credentials } from '@grpc/grpc-js';
+
 import { MatchesClient } from './matches/matches_grpc_pb.js';
 import pb from './matches/matches_pb.js';
 
 const client = new MatchesClient(
-  'localhost:50051',
-  credentials.createInsecure(),
-  {}
+    'localhost:50051',
+    credentials.createInsecure()
 );
 
 const request = new pb.GetMatchByIdRequest();
