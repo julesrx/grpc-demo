c59c54fb06d962a43f75c88c400ecc684f77d6e5
julesrx <dev@julesrx.fr>
2024-03-01 10:58:09 +0100

    client server ok

--- dotnet/Client/Program.cs
+++ dotnet/Client/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Grpc.Net.Client;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5255");
+var client = new Matches.MatchesClient(channel);
+
+var response = await client.GetMatchByIdAsync(new GetMatchByIdRequest { Id = 178471616 });
+
+Console.WriteLine(JsonSerializer.Serialize(response));
